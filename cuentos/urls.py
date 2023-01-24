ccec7dba3a2d88bd07419cf439d31caa334d82ec
Leo <leocasti2@gmail.com>
2023-01-23 21:04:18 -0500

    añadida la plantilla y css para detail

--- cuentos/urls.py
+++ cuentos/urls.py
@@ -3,12 +3,12 @@
 from django.urls import path
 from .views import detail, signout, signup,signin,index
 
-
+app_name = 'cuentos'
 urlpatterns = [
     path("", index, name="home"),
     path("cuentos/", index, name="cuentos"),
     path("logout/", signout, name="logout"),
     path("signup/", signup, name="signup"),
     path("login/", signin, name="login"),
-    path("cuentos/<int:cuento_id>", detail, name="login")
+    path("cuentos/<int:cuento_id>", detail, name="detail")
 ]
